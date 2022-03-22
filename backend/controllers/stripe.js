b72f11574c11e8a95023c801c6aaee30588bb561
Emmanuel Egomson <fegomson@gmail.com>
2022-03-22 14:28:10 -0500

    Create login link

--- backend/controllers/stripe.js
+++ backend/controllers/stripe.js
@@ -1,5 +1,6 @@
 import User from "../models/user";
 import Stripe from "stripe";
+import queryString from "query-string";
 
 // initialize stripe
 const stripe = Stripe(process.env.STRIPE_SECRET);
@@ -17,12 +18,25 @@ export const createConnectAccount = async (req, res) => {
     // Save stripe_account_id in the db
     user.stripe_account_id = account.id;
     user.save();
-  } else {
-    return res.status(400).json({
-      message: "You already have a stripe account",
-    });
   }
 
   // Create account link based on stripe_account_id (for frontend to complete onboarding)
+  let accountLink = await stripe.accountLinks.create({
+    account: user.stripe_account_id,
+    refresh_url: process.env.STRIPE_REDIRECT_URL,
+    return_url: process.env.STRIPE_REDIRECT_URL,
+    type: "account_onboarding",
+  });
+
+  // pre-fill any user information
+  accountLink = Object.assign(accountLink, {
+    "stripe_user[email]": user.email || undefined,
+  });
+
+  // Generate link and send to frontend
+  const link = `${accountLink.url}?${queryString.stringify(accountLink)}`;
+  console.log(link);
+  res.send(link);
+
   // Update payment schedule (Optional. default is 2days)
 };
