b95eeffa9f23e3405dc68330e4064a2e1480b781
Emmanuel Egomson <fegomson@gmail.com>
2022-03-21 19:33:05 -0500

    Using stripe in the backend

--- backend/controllers/stripe.js
+++ backend/controllers/stripe.js
@@ -1,4 +1,19 @@
+import User from "../models/user";
+import Stripe from "stripe";
+
+// initialize stripe
+const stripe = Stripe(process.env.STRIPE_SECRET);
+
 export const createConnectAccount = async (req, res) => {
-  console.log(req.user);
-  console.log("You hit create connect account endpoint");
+  // Find user from db
+  const user = await User.findById(req.user._id);
+
+  // If user don't have stripe_account_id, create one
+  const account = await stripe.accounts.create({
+    type: "express",
+  });
+  console.log(account);
+
+  // Create account link based on stripe_account_id (for frontend to complete onboarding)
+  // Update payment schedule (Optional. default is 2days)
 };
